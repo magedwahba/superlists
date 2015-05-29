390c8e96e87d24175c083706c30675fb2b95e41b
magedwahba <maged.wahba@gmail.com>
2015-05-29 17:31:16 -0400

    CH5 #2

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,11 @@ class NewVisitor(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -28,17 +33,11 @@ class NewVisitor(unittest.TestCase):
         )
 
         inputbox.send_keys('Buy peacock feathers')
-        time.sleep(1)
-
         inputbox.send_keys('Use peacock feathers to make a fly')
-        time.sleep(1)
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list('1: Buy peacock feathers')
+        self.check_for_row_in_list('2: Use peacock feathers to make a fly')
 
         # self.fail('Finish the test!')
 
